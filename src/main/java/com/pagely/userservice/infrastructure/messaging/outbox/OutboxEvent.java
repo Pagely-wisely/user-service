998f29e6ae054fe002fdca9828831536b0f2aab0
SEOUNJU PARK <sj.ella1030@gmail.com>
2026-05-07 16:28:46 +0900

    🩹 Fix: OutboxEvent 타입 지정 제거 @Column(columnDefinition = "UUID")제거
    Related to: #55

--- src/main/java/com/pagely/userservice/infrastructure/messaging/outbox/OutboxEvent.java
+++ src/main/java/com/pagely/userservice/infrastructure/messaging/outbox/OutboxEvent.java
@@ -42,7 +42,6 @@ import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 public class OutboxEvent {
 
     @Id
-    @Column(columnDefinition = "UUID")
     @GeneratedValue(strategy = GenerationType.UUID)
     private UUID id;
 
