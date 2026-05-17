064fb9775eb889e008f630d8a53c74b6f5634317
SEOUNJU PARK <sj.ella1030@gmail.com>
2026-05-17 16:09:15 +0900

    ♻️ Refactor: 사용하지 않는 엔드포인트, 의존성 제거
    - auth/token 엔드포인트 제거
    - JWT 관련 의존성 제거
    - 코드 정리
    
    Resolves : #59

--- src/main/java/com/pagely/userservice/UserServiceApplication.java
+++ src/main/java/com/pagely/userservice/UserServiceApplication.java
@@ -1,13 +1,12 @@
 package com.pagely.userservice;
 
 import com.pagely.userservice.infrastructure.config.AdminProperties;
-import com.pagely.userservice.infrastructure.security.JwtProperties;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
 @SpringBootApplication
-@EnableConfigurationProperties({JwtProperties.class, AdminProperties.class})
+@EnableConfigurationProperties({AdminProperties.class})
 public class UserServiceApplication {
 
     public static void main(String[] args) {
